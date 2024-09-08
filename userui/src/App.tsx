562d688a1f74810630006877d798bd7c5942d87c
Ashmi <pushkarbc.11ec@gmail.com>
2024-09-08 07:57:11 +0530

    -setup basic react router for userui
    - added signin and signup page
    - setup router for signin and signup page
    - setup alias for folders

--- userui/src/App.tsx
+++ userui/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Layout } from "./pages/AppRoutes";
 function App() {
-  return <div>ZinFleet</div>;
+  return (
+    <div>
+      <div>routes</div>
+      <BrowserRouter>
+      <Routes>
+        {/* Define the layout route with nested routes */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<SignIn />} /> {/* Default route */}
+          <Route path="/signup" element={<SignUp />} /> {/* Sign Up route */}
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
