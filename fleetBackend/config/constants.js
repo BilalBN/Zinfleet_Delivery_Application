d1cf3d4431407b4680589c6bfd1e590d122cb27b
Deepak KK <Deepak>
2024-12-08 15:40:47 +0530

    Integrated job status api

--- fleetBackend/config/constants.js
+++ fleetBackend/config/constants.js
@@ -1,12 +1,22 @@
-const constants ={
-    shop:'shop',
-    admin:'admin',
-    fleet:'fleet' 
+const constants = {
+    shop: 'shop',
+    admin: 'admin',
+    fleet: 'fleet'
 }
 
 const OTPSERVICE = Object.freeze({
     PHONE: Symbol("phone"),
     EMAIL: Symbol("email")
 });
-module.exports=constants
-module.exports=  OTPSERVICE
+
+const JOBSTATUS = {
+    PENDING: 1,
+    IN_PROGRESS: 2,
+    DELIVERED: 3
+};
+module.exports = {
+    constants,
+    OTPSERVICE,
+    JOBSTATUS
+}
+
